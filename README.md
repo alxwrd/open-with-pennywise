d152bc8c7b3b1b79aaa9b11dd1d4cc9be7cbc048
Alex Ward <alex.w.ward@oracle.com>
2018-12-19 17:20:39 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# <img src="https://i.imgur.com/bKsns66.png" height="37" style="vertical-align: middle;"> Open with Pennywise
+
+[![firefox add-on][firefox-badge]](https://addons.mozilla.org/en-GB/firefox/addon/open_with_pennywise)
+[![chrome extention][chrome-badge]](https://chrome.google.com/webstore/detail/open_with_pennywise)
+
+
+A Chrome/Firefox extension to send a url to [Pennywise](https://github.com/kamranahmedse/pennywise)
+via right click. Works great with Pennywise's _auto-embed videos_ feature for
+Youtube, Vimeo, Twitch, and Daily Motion.
+
+
+[firefox-badge]: https://img.shields.io/badge/firefox-555555.svg?logo=Mozilla%20Firefox
+[chrome-badge]: https://img.shields.io/badge/chrome-555555.svg?logo=Google%20Chrome&logoColor=whitesmoke
---
+++ open-with-pennywise/background.js
@@ -0,0 +1,40 @@
+const browser = window.browser || window.chrome;
+browser.menus = browser.contextMenus;
+
+const pennywiseUrl = "http://localhost:6280";
+const menuItemId = "open-with-pennywise";
+
+var checker = setInterval(ping, 5000);
+
+ping();
+
+function ping() {
+  fetch(pennywiseUrl + "/ping", {
+    method: "GET",
+    mode: "no-cors"
+  })
+    .then(response => {
+      browser.menus.remove(menuItemId, () => {
+        browser.menus.create({
+          id: menuItemId,
+          title: "Open in pennywise",
+          contexts: ["link"]
+        });
+      });
+    })
+    .catch(error => {
+      browser.menus.remove(menuItemId);
+    });
+}
+
+browser.menus.onClicked.addListener((info, tab) => {
+  switch (info.menuItemId) {
+    case menuItemId:
+      fetch(pennywiseUrl + "?url=" + info.linkUrl, {
+        method: "GET",
+        mode: "no-cors"
+      });
+
+      break;
+  }
+});
